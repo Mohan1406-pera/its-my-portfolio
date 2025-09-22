81c8e440ab3ba8501f0a1ec9e622d95549a49b15
Mohan1406-pera <mohankrishnapera567@gmail.com>
2025-09-22 19:37:05 +0530

    First commit

---
+++ Port.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings:["Frontend Developer","ECE Engineer","Problem Solver"],
+    typespeed:100,
+    backspeed:100,
+    backDelay:1000,
+    loop:true
+});
